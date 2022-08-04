910615c38c3972b0e992bf1e23f2f3621b3060bb
Amir Anglo <angloamir@gmail.com>
2022-08-04 07:04:24 +0200

    Up to TS

--- index.ts
+++ index.ts
@@ -7,22 +7,22 @@ function findIndexDameX(valeur) {
     if(dame[e][0] != valeur) return true;
   }
 }
-  
-for(let e = 1; e < 10; e++) {
 
-    listPosXY[e - 1] = [[e, e]]
+
+  for(let e = 1; e < 10; e++) {
 
 dame[e] = [0, e]
  if(findIndexDameX(e)) {
     if((((e - dame[e][0]) + ((e + 1) - dame[e][0]))) <= 9) {
     dame[e] = [(((e - dame[e][0]) + ((e + 1) - dame[e][0]))), e] 
     } else {
-    
+      
     }
   }
 }
 
-console.log(listPosXY, dame)
+console.log(dame)
+
 
 
 // let damepos = (i - 1, i)
